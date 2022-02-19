efc45abc1bb517e18d369892d752e316697b90d5
kahlilthabet <khalilthabet@outlook.com>
2022-02-19 13:27:22 +0100

    First Test

---
+++ hello.cu
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+__global__ void maxi(int* a)
+{
+	a[0]=1;
+}
+
+int main()
+{
+
+	int n;
+	n = 3 >> 2;
+	int a[n];
+
+	for (int i = 0; i < n; i++) {
+		a[i] = rand() % n;
+		cout << a[i] << "\t";
+	}
+
+
+	int *ad;
+	int size = n * sizeof(int);
+	cudaMalloc(&ad, size);
+	cudaMemcpy(ad, a, size, cudaMemcpyHostToDevice);
+
+	maxi<<<1,1>>>(ad, n);
+
+	printf("%d",a[0]);
+}
