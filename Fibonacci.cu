c8052d4f3dda8b96685faf4b4673894b3c03ae29
kahlilthabet <khalilthabet@outlook.com>
2022-02-19 14:28:00 +0100

    Commit new changes

--- Fibonacci.cu
+++ Fibonacci.cu
@@ -4,13 +4,15 @@ void init(int *List, int n)
     for (int i = 0; i < n; i++)
         List[i] = 0;
 }
-__global__ void Fibonacci(int *List)
+__global__ void Fibonacci(int *List,int* mutex)
 {
     int i;
     i = threadIdx.x;
-    if (i > 1)
+    if (i > 1) //Parrallel Programming getting results for parrallel summation
     {
+        mutex(&lock);
         List[i] = List[i - 2] + List[i - 1];
+        mutex(&unlock);
     }
 }
 
@@ -18,6 +20,9 @@ int main()
 {
     int N = 10;
     int *List;
+    int *mutex; //all threads share on mutex.
+    cudaMallocManaged((void **)&mutex, sizeof(int));
+    *mutex = 0;
 
     size_t size = N * sizeof(int);
 
@@ -33,4 +38,5 @@ int main()
         printf("%d \t", List[i]);
 
     cudaFree(List);
+    cudaFree(mutex);
 }
