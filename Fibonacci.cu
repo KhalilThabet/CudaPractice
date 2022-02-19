bff47128f0a8d0a8ed9f6bbe24e71c5d91ded268
kahlilthabet <khalilthabet@outlook.com>
2022-02-19 14:23:57 +0100

    Commit new changes

--- Fibonacci.cu
+++ Fibonacci.cu
@@ -1,13 +1,17 @@
 #include <stdio.h>
-void init(int* List,int n){
-    for (int i=0;i<n;i++) List[i]=0;
+void init(int *List, int n)
+{
+    for (int i = 0; i < n; i++)
+        List[i] = 0;
 }
 __global__ void Fibonacci(int *List)
 {
     int i;
     i = threadIdx.x;
     if (i > 1)
-        List[i] = List[i - 1] + List[i - 2];
+    {
+        List[i] = List[i - 2] + List[i - 1];
+    }
 }
 
 int main()
@@ -18,9 +22,9 @@ int main()
     size_t size = N * sizeof(int);
 
     cudaMallocManaged(&List, size);
-    init(List,N);
+    init(List, N);
     List[0] = 1;
-    List[1] = 1;
+    List[1] = 2;
     size_t threads_per_block = 10;
 
     Fibonacci<<<1, threads_per_block>>>(List);
