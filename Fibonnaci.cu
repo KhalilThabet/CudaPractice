3cfb24919eaa8ea1ad1bd9a95a6a67b8f32a9ded
kahlilthabet <khalilthabet@outlook.com>
2022-02-19 14:13:18 +0100

    Commit new changes

---
+++ Fibonnaci.cu
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+__global__ void Fibonacci(int *List)
+{
+    int i;
+    i = threadIdx.x;
+    if (i > 1)
+        List[i] = List[i - 1] + List[i - 2];
+}
+
+int main()
+{
+    int N = 10;
+    int *List;
+
+    size_t size = N * sizeof(int);
+
+    cudaMallocManaged(&List, size);
+    List[0] = 1;
+    List[1] = 1;
+    size_t threads_per_block = 10;
+
+    doubleElements<<<1, threads_per_block>>>(List);
+    cudaDeviceSynchronize();
+    for (int i = 0; i < N; i++)
+        printf("%d \t", List[i]);
+
+    cudaFree(List);
+}
